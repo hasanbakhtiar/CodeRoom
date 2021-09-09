023c01d685d3902e9de61391d82a4c4bcae681ec
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-09-09 18:37:06 +0400

    JS Lesson1

---
+++ JS/Lesson1/main.js
@@ -0,0 +1,21 @@
+//one line comment
+
+
+/*
+more line 
+comments
+*/
+
+
+
+// debugger;
+
+// console.log("Hello JavaScript");
+// console.log("Hello JavaScript");
+
+// consolelog("Hello JavaScript");
+// console.log("Hello JavaScript");
+// console.log("Hello JavaScript");
+
+// alert("Hello Front");
+// window.alert("Front Hello");
