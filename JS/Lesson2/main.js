bf100b6e45b88d6182f84905e99b78ff4cb85a4e
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-09-13 15:05:19 +0400

    js lesson2

---
+++ JS/Lesson2/main.js
@@ -0,0 +1,85 @@
+// Primitive Data Type
+    // string
+        // var infoData = "Hello JS";
+        // var infoData1 = 'Hello JS1';
+    // number
+        // var infoData = 5;
+        // var infoData1 = 15;//number
+    // boolean
+        // true; false;
+
+        // if (false) {
+        //     console.log('This is True');
+        // }else{
+        //     console.log('This is False');
+        // }
+
+// Composite Data Type
+    // array
+        // var infoData = ['Hello JavaScript', 5, true, "hi"];
+        // var infoData = new Array();
+    // object
+        // var infoCar = {
+        //     model: "bmw",
+        //     color: "white",
+        //     year: 2021
+        // };
+        // var infoData = new Object();
+
+
+// Special Data Type
+    // undefined
+        var infoData = undefined;
+    // null
+        var infoData = null;
+
+
+
+
+// var a = "Hello JavaScript";
+
+// console.log(infoCar.year);
+console.log(infoData);
+// console.log(infoData+infoData1);
+
+
+
+
+// All Data Type 
+// --------------------------------------------------------------------------------------------------
+// Primitive (Bəsit)  Data(Data) Type(Tipi) Start
+var infomationText1   = "Salam1";     //String(mətn)      data type ("Hi")
+var infomationText2   = 'Salam2';     //String(mətn)      data type ("Hi")
+var infomationNumb    = 5 ;           //Number(rəqəm)     data type (5)
+var infomationBoolean = true ;        //Boolean(məntiqi)  data type (true/flase)
+
+document.write(infomationText1     + "<br>");
+document.write(infomationText2     + "<br>");
+document.write(infomationNumb      + "10" + "<br>");
+document.write(infomationBoolean   + "<br>");
+// Basic(Bəsit)        Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Composite(Birləşik) Data(Data) Type(Tipi) Start
+var infomationArray  =  ['pc',5,true,'table'];    //Array
+var infomationObject =  {
+    name:"Samsung",
+    ROM: "OneUI"
+};                                                //Object
+
+document.write(infomationArray          + "<br>");
+document.write(infomationObject.name    + "<br>");
+document.write(infomationObject.ROM     + "<br>");
+// Composite(Birləşik) Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Special(Xüsusi)     Data(Data) Type(Tipi) Start
+var informationUndefined = "";   //Undefined
+var informationNULL      = null; //Null
+
+document.write(informationUndefined          + "<br>");
+document.write(informationNULL               + "<br>");
+// Special(Xüsusi)     Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
