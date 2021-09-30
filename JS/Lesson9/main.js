b33d72d6035441b5b70127a05b965d41753440ea
Hasan <hasan.doctype@gmail.com>
2021-09-30 18:47:32 +0400

    add Functions

---
+++ JS/Lesson9/main.js
@@ -0,0 +1,33 @@
+// const myFunc = function(){
+//         document.write("Hello My Function");
+// }
+
+
+// function myFunc(){
+//         document.write("Hello My Function");
+// }
+
+// myFunc();
+
+
+// function infoData (c=5,e=10){
+//     // let a = 10;
+//     // let b = 20;
+//     document.write(c+e);
+// }
+
+// infoData(10,40);
+
+// (function infoData(a){
+//     document.write(a);
+// })(20)
+
+
+// ES5 END
+
+// ES6 Start
+
+const infoData =()=>{
+    document.write("Hello ES6");
+}
+infoData();
