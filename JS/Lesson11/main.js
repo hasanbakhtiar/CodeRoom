fa1e1d4b0b5601310db40b3b980e8eb14a886a74
Hasan <hasan.doctype@gmail.com>
2021-10-07 19:01:50 +0400

    DOM Lesson2

---
+++ JS/Lesson11/main.js
@@ -0,0 +1,5 @@
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval_clearinterval2
+// https://www.w3schools.com/jsref/met_win_setinterval.asp
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout2
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout3
+// https://www.w3schools.com/jsref/met_win_settimeout.asp
