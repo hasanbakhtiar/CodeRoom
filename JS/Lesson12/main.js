0640d603bb18e54fb28c865c3f3ee6d5aa643d78
Hasan <hasan.doctype@gmail.com>
2021-10-11 18:49:38 +0400

    lesson13

---
+++ JS/Lesson12/main.js
@@ -0,0 +1,3 @@
+// https://www.w3schools.com/js/js_htmldom_events.asp
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_events_onchange
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_events_mousedown
---
+++ JS/Lesson13/main.js
@@ -0,0 +1,3 @@
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_addeventlistener_usecapture
+// https://www.w3schools.com/js/js_htmldom_navigation.asp
+// https://www.w3schools.com/js/js_htmldom_nodes.asp
