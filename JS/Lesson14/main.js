4cc9865d16597d63dfc63ffc36716acaa95c26c7
Hasan <hasan.doctype@gmail.com>
2021-10-14 18:39:04 +0400

    BOM Lesson1

---
+++ JS/Lesson14/main.js
@@ -0,0 +1,6 @@
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_win_inner
+// https://www.w3schools.com/Jsref/tryit.asp?filename=tryjsref_win_close
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_resizeto
+// https://www.w3schools.com/Jsref/met_win_moveto.asp
+
+// https://www.w3schools.com/js/js_window_location.asp
